d12fc0f33e0fa7b67dd2060602ce0f1efea3f642
yamtimor <yamtimor@gmail.com>
2022-09-03 14:55:43 +0300

    some small changes

--- src/scraper/data_scraper_config.py
+++ src/scraper/data_scraper_config.py
@@ -1,11 +1,14 @@
 from pprint import pprint
+from pydantic import BaseSettings
 
-class ScraperConfig:
+class ScraperConfig(BaseSettings):
 
     base_url: str = 'https://imslp.org/'
     uri: str = 'wiki/List_of_works_by_Johann_Sebastian_Bach'
 
+
     def __call__(self):
+        print(self.base_url + self.uri)
         return self.base_url + self.uri
 
 data = ScraperConfig()
