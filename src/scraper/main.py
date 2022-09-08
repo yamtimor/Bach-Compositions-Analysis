381ef38b401ac2eb39a808bf6b8393aad5401805
yamtimor <yamtimor@gmail.com>
2022-09-08 16:45:51 +0300

    fixing the errors I had in main

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -3,11 +3,11 @@ from data_scraper_config import ScraperConfig
 from pprint import pprint
 
 def main():
-    pass
 
     data = ScraperConfig()
-    pprint(data.full_url)
-    DataPuller.get_data(data.full_url)
+    # pprint(data.full_url)
+    Scraper = DataPuller(data.full_url)
+    pprint(Scraper.get_data())
 
 
 if __name__ == "__main__":
